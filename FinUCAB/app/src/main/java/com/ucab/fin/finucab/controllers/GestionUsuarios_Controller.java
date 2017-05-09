3f17ea73fbae4af8403729627849363ea2c929e1
Garry Jr. Bruno <garryjrbruno@hotmail.com>
2017-05-09 19:08:28 -0400

    Agregue las excepciones de Longitud de caracteres

--- FinUCAB/app/src/main/java/com/ucab/fin/finucab/controllers/GestionUsuarios_Controller.java
+++ FinUCAB/app/src/main/java/com/ucab/fin/finucab/controllers/GestionUsuarios_Controller.java
@@ -5,6 +5,7 @@ import android.widget.EditText;
 import com.ucab.fin.finucab.exceptions.CampoVacio_Exception;
 import com.ucab.fin.finucab.exceptions.ContrasenasDiferentes_Exception;
 import com.ucab.fin.finucab.exceptions.CorreoInvalido_Exception;
+import com.ucab.fin.finucab.exceptions.Longitud_Exception;
 
 /**
  * Created by Junior on 06/05/2017.
@@ -33,13 +34,19 @@ public class GestionUsuarios_Controller {
 
         try{
             verificoVacio(nombre);
+            verificoLongitud(nombre,50,"string");
             verificoVacio(apellido);
+            verificoLongitud(apellido,50,"string");
             verificoVacio(correo);
+            verificoLongitud(correo,100,"string");
             verificoCorreo(correo);
         }catch (CampoVacio_Exception e){
             e.getCampo().setError(e.getMessage());
             return 1;
         } catch (CorreoInvalido_Exception e){
+            e.getCampo().setError(e.getMessage());
+            return 1;
+        }catch (Longitud_Exception e){
             e.getCampo().setError(e.getMessage());
             return 1;
         }
@@ -56,13 +63,17 @@ public class GestionUsuarios_Controller {
     {
           try{
               verificoVacio(usuario);
+              verificoLongitud(usuario,50,"string");
               verificoVacio(contrasena1);
               verificoVacio(contrasena2);
               verificoIgualdad(contrasena1,contrasena2);
-          }catch (CampoVacio_Exception e){
+          } catch (CampoVacio_Exception e){
               e.getCampo().setError(e.getMessage());
               return 1;
           } catch (ContrasenasDiferentes_Exception e){
+              e.getCampo().setError(e.getMessage());
+              return 1;
+          } catch (Longitud_Exception e){
               e.getCampo().setError(e.getMessage());
               return 1;
           }
@@ -78,8 +89,13 @@ public class GestionUsuarios_Controller {
     public static int validacionEtapaSeguridad(){
         try{
             verificoVacio(pregunta);
+            verificoLongitud(pregunta,1000,"string");
             verificoVacio(respuesta);
+            verificoLongitud(respuesta,1000,"string");
         }catch (CampoVacio_Exception e){
+            e.getCampo().setError(e.getMessage());
+            return 1;
+        }catch (Longitud_Exception e){
             e.getCampo().setError(e.getMessage());
             return 1;
         }
@@ -87,6 +103,21 @@ public class GestionUsuarios_Controller {
         return 0;
     }
 
+    //Validacion para verficar que los campos cumplan con el rango correcto
+    public static void verificoLongitud(EditText campo,int longitud, String tipo)throws Longitud_Exception {
+           if (campo.getText().toString().length() >= longitud) {
+               if (tipo.equals("string")) {
+                   Longitud_Exception campolargo = new Longitud_Exception("El texto es demasiado largo");
+                   campolargo.setCampo(campo);
+                   throw campolargo;
+               }
+               if (tipo.equals("int")) {
+                   Longitud_Exception campolargo = new Longitud_Exception("El numero es muy alto");
+                   campolargo.setCampo(campo);
+                   throw campolargo;
+               }
+           }
+    }
 
     //Realizo la validacion para verificar que el campo este vacio:
     public static void verificoVacio(EditText campo) throws CampoVacio_Exception {
@@ -101,7 +132,7 @@ public class GestionUsuarios_Controller {
 
     //Realizo la validacion para verificar que las contraseñas son diferentes:
     public static void verificoIgualdad(EditText contesena1, EditText contrasena2) throws ContrasenasDiferentes_Exception {
-        if (!(contrasena1.getText().toString().equals(contrasena2.getText().toString())))
+        if (!(contrasena2.getText().toString().equals(contrasena2.getText().toString())))
         {
             ContrasenasDiferentes_Exception diferentes = new ContrasenasDiferentes_Exception("Las contraseñas son diferentes");
             diferentes.setCampo(contrasena2);
@@ -112,13 +143,13 @@ public class GestionUsuarios_Controller {
 
     //Realizo la validacion para verificar si el correo electronico suministrado es valido.
     public static void verificoCorreo(EditText campo)throws CorreoInvalido_Exception{
-        if (!correo.getText().toString().contains("@")) {
+        if (!campo.getText().toString().contains("@")) {
             CorreoInvalido_Exception correoinvalido  =  new CorreoInvalido_Exception("El correo es invalido");
             correoinvalido.setCampo(campo);
             throw correoinvalido;
         }else
         {
-            String [] fragmento = correo.getText().toString().split("@");
+            String [] fragmento = campo.getText().toString().split("@");
             if (!fragmento[1].contains(".")) {
                 CorreoInvalido_Exception correoinvalido  =  new CorreoInvalido_Exception("El correo es invalido");
                 correoinvalido.setCampo(campo);
