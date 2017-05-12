c81913f6e67d1051fc9a719cfd074a039dd7947a
juanp22 <juan12413@gmail.com>
2017-05-12 15:32:16 -0400

    AgregarCategoria
    -AgregarCategoria_Fragment
    -fragment_agregar_categoria
    -strings.xml

--- FinUCAB/app/src/main/java/com/ucab/fin/finucab/fragment/CategoryFragment.java
+++ FinUCAB/app/src/main/java/com/ucab/fin/finucab/fragment/CategoryFragment.java
@@ -1,9 +1,11 @@
 package com.ucab.fin.finucab.fragment;
 
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,20 +13,69 @@ import android.view.ViewGroup;
 import com.ucab.fin.finucab.R;
 import com.ucab.fin.finucab.activity.MainActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryFragment extends Fragment {
+
 
     private View parentView;
     private MainActivity parentActivity;
-    private AppBarLayout appBar;
+    private TabLayout pestanas;
+    private ViewPager viewPager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        parentView = inflater.inflate(R.layout.category_fragment, container, false);
+        View view= inflater.inflate(R.layout.category_fragment, container, false);
         parentActivity = (MainActivity) getActivity();
-        //appBar = (AppBarLayout) parentView.findViewById(R.id.appbar);
-        //appBar.enable
+        parentActivity.getSupportActionBar().setTitle("Categorias");
 
+        if (savedInstanceState == null) {
+            viewPager = (ViewPager) view.findViewById(R.id.category);
+            AdaptadorSecciones adapter = new AdaptadorSecciones(getChildFragmentManager());
+            adapter.addFragment(new AgregarCategoria_Fragment(), "Agregar Categoria");
+            viewPager.setAdapter(adapter);
 
-        return parentView;
+        }
+
+        return view;
     }
+
+
+
+    /**
+     * Un {@link FragmentStatePagerAdapter} que gestiona las secciones, fragmentos y
+     * títulos de las pestañas
+     */
+    public class AdaptadorSecciones extends FragmentStatePagerAdapter {
+        private final List<Fragment> fragmentos = new ArrayList<>();
+        private final List<String> titulosFragmentos = new ArrayList<>();
+
+        public AdaptadorSecciones(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            return fragmentos.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentos.size();
+        }
+
+        public void addFragment(android.support.v4.app.Fragment fragment, String title) {
+            fragmentos.add(fragment);
+            titulosFragmentos.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titulosFragmentos.get(position);
+        }
+    }
+
+
 
 }
