5f350e4cbfdc4c4a829a0153aae92025edcfc22c
Garry Jr. Bruno <garryjrbruno@hotmail.com>
2017-05-11 20:50:47 -0400

    Solucionado problemas de vaidacion de Correo

---
+++ FinUCAB/app/src/main/java/com/ucab/fin/finucab/activity/BeginActivity.java
@@ -0,0 +1,15 @@
+package com.ucab.fin.finucab.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ucab.fin.finucab.R;
+
+public class BeginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin);
+    }
+}
