ecdc2fab8b7c07ccf9c4393c7dfd6e6649f01ddb
gusth <mrgusth@gmail.com>
2017-05-12 17:59:39 -0400

    homepage y edicion perfil 2de10

--- FinUCAB/app/src/main/java/com/ucab/fin/finucab/fragment/MyProfileFragment.java
+++ FinUCAB/app/src/main/java/com/ucab/fin/finucab/fragment/MyProfileFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.ucab.fin.finucab.R;
 import com.ucab.fin.finucab.activity.MainActivity;
@@ -16,11 +17,23 @@ public class MyProfileFragment extends Fragment {
     private View parentView;
     private MainActivity parentActivity;
 
+    EditText etNombre;
+    EditText etApellido;
+    EditText etCorreo;
+    EditText etUsuario;
+    EditText etClave;
+    EditText etPregunta;
+    EditText etRespuesta;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
         parentView = inflater.inflate(R.layout.profile_fragment, container, false);
         parentActivity = (MainActivity) getActivity();
         return parentView;
+
+
     }
 
 }
