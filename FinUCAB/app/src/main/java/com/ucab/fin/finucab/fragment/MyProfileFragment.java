076a9f4eee1f1fc732d3315a149b77cacda77226
gusth <mrgusth@gmail.com>
2017-05-13 09:34:56 -0400

    nada nuevo

--- FinUCAB/app/src/main/java/com/ucab/fin/finucab/fragment/MyProfileFragment.java
+++ FinUCAB/app/src/main/java/com/ucab/fin/finucab/fragment/MyProfileFragment.java
@@ -3,9 +3,12 @@ package com.ucab.fin.finucab.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.ucab.fin.finucab.R;
@@ -24,14 +27,28 @@ public class MyProfileFragment extends Fragment {
     EditText etClave;
     EditText etPregunta;
     EditText etRespuesta;
-
+    Button boton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
-        parentView = inflater.inflate(R.layout.profile_fragment, container, false);
-        parentActivity = (MainActivity) getActivity();
-        return parentView;
+        View view = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        boton = (Button) view.findViewById(R.id.bBack);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager man = getFragmentManager();
+                FragmentTransaction trans = man.beginTransaction();
+                HomeFragment profi = new HomeFragment();
+                trans.add(R.id.drawer_layout,profi);
+                trans.commit();
+
+            }
+        });
+
+        return view;
 
 
     }
