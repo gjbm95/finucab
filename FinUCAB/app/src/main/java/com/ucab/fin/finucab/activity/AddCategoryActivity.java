7dd39851e540cf46812f9febb5c3f44aa2bdb3e0
juanp22 <juan12413@gmail.com>
2017-05-15 22:37:44 -0400

    Arreglando boton de Volver y Aceptar
    -Intentando colcoarle funcionalidad a los botones

--- FinUCAB/app/src/main/java/com/ucab/fin/finucab/activity/AddCategoryActivity.java
+++ FinUCAB/app/src/main/java/com/ucab/fin/finucab/activity/AddCategoryActivity.java
@@ -2,39 +2,60 @@ package com.ucab.fin.finucab.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.ucab.fin.finucab.R;
 
-public class AddCategoryActivity extends AppCompatActivity  {
-    private Button anterior;
+public class AddCategoryActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    Button signInButton;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_agregar_categoria);
 
-        //Colocando acciones al boton de "Cancelar":
-        //----------------------------------------------------------------------------------
-        anterior = (Button) findViewById(R.id.backButton);
-        anterior.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            //Cambio la actividad de vuelta al Home Inicio de Sesion.
-                                            if (anterior.getText().equals("Volver")) {
-                                                Intent home = new Intent(AddCategoryActivity.this, LoginActivity.class);
-                                                startActivity(home);
-                                               finish();
-                                            }
+        //Colocando el icono en la parte superior izquierda:
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.logoh);
+        actionBar.setTitle("");
+        //------------------------------------------------------------------------------------------
+//        BIND VIEES (Se extraen los objetos asociados a los botones en pantalla)
+        signInButton = (Button) findViewById(R.id.backButton);
+        signUpButton = (Button) findViewById(R.id.acceptButton);
 
-                                        }
-                                    }
-        );
+//        SET LISTENERS (Se le asigna la actividad en el cual funcionaran)
+        signInButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
 
     }
 
 
 
+
+
+
+    //Dandole funcionalidades a cada uno de los botones que salen en pantalla:
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        switch (view.getId()){
+
+            case R.id.backButton:
+                i = new Intent(AddCategoryActivity.this, MainActivity.class);
+                startActivity(i);
+                break;
+            //Al accionar, se inicia la actividad que presenta el formulario de registro.
+            case R.id.acceptButton:
+                i = new Intent(AddCategoryActivity.this, RegisterActivity.class);
+                startActivity(i);
+                break;
+        }
+
+    }
 }
