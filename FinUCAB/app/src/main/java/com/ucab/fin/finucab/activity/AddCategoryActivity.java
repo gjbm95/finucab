e79136361a8b671713f15e39e7478a3e6e4a1bc9
juanp22 <juan12413@gmail.com>
2017-05-17 00:40:56 -0400

    Funcionalidad Boton Exportar
    -Enlace el boton Exportar hacia otra vista para probar

--- FinUCAB/app/src/main/java/com/ucab/fin/finucab/activity/AddCategoryActivity.java
+++ FinUCAB/app/src/main/java/com/ucab/fin/finucab/activity/AddCategoryActivity.java
@@ -54,12 +54,12 @@ public class AddCategoryActivity extends AppCompatActivity implements View.OnCli
         switch (view.getId()){
 
             case R.id.backButton:
-                i = new Intent(AddCategoryActivity.this, ListaCategoryActivity.class);
+                i = new Intent(AddCategoryActivity.this, MainActivity.class);
                 startActivity(i);
                 break;
             //Al accionar, se inicia la actividad que presenta el formulario de registro.
             case R.id.acceptButton:
-                i = new Intent(AddCategoryActivity.this,ListaCategoryActivity.class);
+                i = new Intent(AddCategoryActivity.this,MainActivity.class);
                 startActivity(i);
                 break;
         }
