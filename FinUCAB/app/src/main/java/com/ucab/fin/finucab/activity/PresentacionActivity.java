5e81f693c0665ae32039993cbb66b5f5ee7bb449
OswalLopezM <oswaldo7365@hotmail.com>
2017-05-31 00:27:19 -0400

    ULTIMO COMMIT

--- FinUCAB/app/src/main/java/com/ucab/fin/finucab/activity/PresentacionActivity.java
+++ FinUCAB/app/src/main/java/com/ucab/fin/finucab/activity/PresentacionActivity.java
@@ -1,4 +1,4 @@
-﻿package com.ucab.fin.finucab.activity;
+package com.ucab.fin.finucab.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -42,7 +42,7 @@ public class PresentacionActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         //Configuraciones de Red: (Coloque aqui la IP y puerto de su servidor)
         //-------------------------------------------------------------------
-            Parametros.setServer("http://192.168.1.101"); // Asigno direccion IP a parametros de red.
+            Parametros.setServer("http://192.168.1.4"); // Asigno direccion IP a parametros de red.
             Parametros.setPuerto("8080"); // Asigno puerto por el cual el servidor escucha.
         if(datos.equals("vacio")) {
             //-------------------------------------------------------
