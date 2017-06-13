9240c3399b61a94d6e0d17713f8cdaace623b46b
Garry Jr. Bruno <garryjrbruno@hotmail.com>
2017-06-13 17:23:01 -0400

    Creando area para colocar procedimientos almacenados en la Base de datos

---
+++ Basededatos/Procedimientos/Procedimientos.sql
@@ -0,0 +1,41 @@
+--- PROCEDIMIENTOS PARA EL MODULO 1:
+
+
+
+--- [Coloquen aqui sus procedimientos]
+
+
+
+--- PROCEDIMIENTOS PARA EL MODULO 2:
+
+
+--- [Coloquen aqui sus procedimientos]
+
+
+--- PROCEDIMIENTOS PARA EL MODULO 3:
+
+
+--- [Coloquen aqui sus procedimientos]
+
+
+
+--- PROCEDIMIENTOS PARA EL MODULO 4:
+
+
+
+--- [Coloquen aqui sus procedimientos]
+
+
+
+--- PROCEDIMIENTOS PARA EL MODULO 5:
+
+
+
+--- [Coloquen aqui sus procedimientos]
+
+
+
+--- PROCEDIMIENTOS PARA EL MODULO 6:
+
+
+--- [Coloquen aqui sus procedimientos]
