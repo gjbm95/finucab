97f9c803f60eeb0aef4b84a1784b2bbe11004cf2
Garry Jr. Bruno <garryjrbruno@hotmail.com>
2017-06-14 00:45:32 -0400

    Agregando Fragments de Modulo 2

--- FinUCAB/app/src/main/java/com/ucab/fin/finucab/fragment/HomeFragment.java
+++ FinUCAB/app/src/main/java/com/ucab/fin/finucab/fragment/HomeFragment.java
@@ -18,44 +18,17 @@ public class HomeFragment extends Fragment {
     private View parentView;
     private MainActivity parentActivity;
 
-    Button boton;
-    /*@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        parentView = inflater.inflate(R.layout.home_fragment, container, false);
-        parentActivity = (MainActivity) getActivity();
-        return parentView;
-
-    }*/
 
     @Override  //METODO QUE LLAMA A OTROS FRAGMENTS(PUEDE SER USADO TANTO EN FRAGMENTS COMO EN ACTIVITY)
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
         View view = inflater.inflate(R.layout.home_fragment, container, false);
 
-        boton = (Button) view.findViewById(R.id.bAffiliations);
-
-        boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager man = getFragmentManager();
-                FragmentTransaction trans = man.beginTransaction();
-                Afiliaciones_Fragment afi = new Afiliaciones_Fragment();
-                trans.add(R.id.drawer_layout,afi);
-                //trans.replace(R.id.drawer_layout, afi);
-                trans.commit();
-
-            }
-        });
-
         return view;
 
     }
 
-    /*FragmentManager man = getFragmentManager();
-    FragmentTransaction trans = man.beginTransaction();
-    Afiliaciones_Fragment afi = new Afiliaciones_Fragment();
-                trans.replace(R.id.contenedor_principal, afi);*/
 
 
 }
